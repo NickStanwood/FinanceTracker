8a0ad00068a213f463a49bb5f564f1aa90bbd739
Nick Stanwood <nstanwood1@gmail.com>
2024-01-30 21:04:51 -0700

    WIP added transactionConverterViewModel

---
+++ FinanceTracker/FinanceTracker.WPF/ViewModels/TransactionConverterViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using FinanceTracker.Models;
+
+namespace FinanceTracker.WPF
+{
+    internal class TransactionConverterViewModel : ViewModelBase<AccountModel>
+    {
+        private string _rawTransFilePath;
+        public string RawTransFilePath { get { return _rawTransFilePath; } set { _rawTransFilePath = value; Notify(); } }
+        public ObservableCollection<RawTransactionModel> RawTransactions { get; set; } = new ObservableCollection<RawTransactionModel>();
+
+        public ObservableCollection<TransactionModel> Transactions { get; set; } = new ObservableCollection<TransactionModel>();
+
+        public LamdaCommand ConvertCmd { get; set; }
+        public LamdaCommand BrowseCmd { get; set; }
+        public LamdaCommand ViewRulesCmd { get; set; }
+        public LamdaCommand SaveCmd { get; set; }
+        public TransactionConverterViewModel(AccountModel model) : base(model) { }
+        public TransactionConverterViewModel() : base() { }
+        protected override void Initialize()
+        {
+            //BrowseCmd
+
+            ConvertCmd = new LamdaCommand(
+                (obj) => File.Exists(RawTransFilePath),
+                (obj) => ConvertTransactions()
+            );
+
+            //ViewRulesCmd
+
+            //SaveCmd
+        }
+
+        private void ConvertTransactions()
+        {
+
+        }
+    }
+}
